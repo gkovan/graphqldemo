51fa40e46384b511d8682288f4e77921da9824bc
gkovan <gkovan@hotmail.com>
2021-01-22 14:13:14 -0500

    sample working

---
+++ okta/samples-java-spring-1.0.0/resource-server/src/main/java/com/okta/spring/example/ResourceServerExampleApplication.java
@@ -0,0 +1,79 @@
+package com.okta.spring.example;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class ResourceServerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerExampleApplication.class, args);
+    }
+
+    @Configuration
+    static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+                .oauth2ResourceServer().jwt();
+
+            // process CORS annotations
+            http.cors();
+
+            // force a non-empty response body for 401's to make the response more browser friendly
+            Okta.configureResourceServer401ResponseBody(http);
+        }
+    }
+
+    @RestController
+    @CrossOrigin(origins = "http://localhost:8080")
+    public class MessageOfTheDayController {
+
+        @GetMapping("/api/userProfile")
+        @PreAuthorize("hasAuthority('SCOPE_profile')")
+        public Map<String, Object> getUserDetails(JwtAuthenticationToken authentication) {
+            return authentication.getTokenAttributes();
+        }
+
+        @GetMapping("/api/messages")
+        @PreAuthorize("hasAuthority('SCOPE_email')")
+        public Map<String, Object> messages() {
+
+            Map<String, Object> result = new HashMap<>();
+            result.put("messages", Arrays.asList(
+                    new Message("I am a robot."),
+                    new Message("Hello, world!")
+            ));
+
+            return result;
+        }
+    }
+
+    class Message {
+        public Date date = new Date();
+        public String text;
+
+        Message(String text) {
+            this.text = text;
+        }
+    }
+}
